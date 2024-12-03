c6d2d3fed3252fb83f07a71e898964174d1a1986
Alex Diaz (Electrofantasia) <mrplanning2@gmail.com>
2024-12-03 13:42:17 +0100

    Header styles added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  Nombre: string = ""
+  Edad: string = ""
+  Ubicacion: string = ""
 }

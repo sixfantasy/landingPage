9a033dc661fc4ddee4a97503fcf81873f4d96e49
Alex Diaz (Electrofantasia) <mrplanning2@gmail.com>
2024-12-09 13:27:24 +0100

    Merge conflicts resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { ProductosComponent } from './productos/productos.component';
 import { ButtonchangeComponent } from './buttonchange/buttonchange.component';
-=======
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
->>>>>>> 22b8898b41484e52de17e8ef4212b906bac5b6aa
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     ProductosComponent,
-    ButtonchangeComponent
-=======
+    ButtonchangeComponent,
     HeaderComponent,
     AboutComponent
->>>>>>> 22b8898b41484e52de17e8ef4212b906bac5b6aa
   ],
   imports: [
     BrowserModule,
